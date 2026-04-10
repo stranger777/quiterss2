b35742a99f92f3038d4e1a23baa541e675f243f4
Ivan Novikov <noviko-iva-money@yandex.ru>
2026-04-10 15:09:07 +0500

    Fix AppVeyor: use batch file so vcvarsall env persists for qmake

---
+++ tools/build/appveyor-ci.bat
@@ -0,0 +1,11 @@
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+set PATH=%QTDIR%\bin;%CD%\bin;%PATH%
+mkdir build
+cd build
+qmake CONFIG+=release -spec win32-msvc ..\quiterss2.pro
+if %errorlevel% neq 0 exit /b %errorlevel%
+nmake
+if %errorlevel% neq 0 exit /b %errorlevel%
+cd ..
+windeployqt build\bin --qmldir=resources\qml --no-compiler-runtime
