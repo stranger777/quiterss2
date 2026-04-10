6a576a0384f63b48bbe4defeeed1a165d39610f8
Ivan Novikov <noviko-iva-money@yandex.ru>
2026-04-10 15:18:47 +0500

    Fix AppVeyor: add .qmake.conf to set QMAKE_MSC_VER=1929 (VS2019) for qmake

--- tools/build/appveyor-ci.bat
+++ tools/build/appveyor-ci.bat
@@ -1,6 +1,7 @@
 @echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 set PATH=%QTDIR%\bin;%CD%\bin;%PATH%
+
 mkdir build
 cd build
 qmake CONFIG+=release -spec win32-msvc ..\quiterss2.pro
