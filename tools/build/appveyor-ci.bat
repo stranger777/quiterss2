7d52466ae0de4adff4951a1e2008b149a19a950e
Ivan Novikov <noviko-iva-money@yandex.ru>
2026-04-10 20:45:24 +0500

    Qt 6.8.2 CI: 64-bit Python on AppVeyor, WebEngine on both

--- tools/build/appveyor-ci.bat
+++ tools/build/appveyor-ci.bat
@@ -1,12 +1,12 @@
 @echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 set PATH=%QTDIR%\bin;%CD%\bin;%PATH%
 
 mkdir build
 cd build
-qmake CONFIG+=release -spec win32-msvc DISABLE_BROWSER=true ..\quiterss2.pro
+qmake CONFIG+=release -spec win32-msvc ..\quiterss2.pro
 if %errorlevel% neq 0 exit /b %errorlevel%
 nmake
 if %errorlevel% neq 0 exit /b %errorlevel%
 cd ..
-windeployqt build\bin --qmldir=%APPVEYOR_BUILD_FOLDER%\resources\qml --no-compiler-runtime
+windeployqt build\bin --qmldir=%APPVEYOR_BUILD_FOLDER%\resources\qml --webengine --no-compiler-runtime
