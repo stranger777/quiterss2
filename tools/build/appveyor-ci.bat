c7c93e1c58353b43b27090070b846348d3baea4c
Ivan Novikov <noviko-iva-money@yandex.ru>
2026-04-10 20:23:51 +0500

    AppVeyor: back to Qt 5.15.2 (aqtinstall v3.3 fails with Qt 6.8.2 on 32-bit Python)

--- tools/build/appveyor-ci.bat
+++ tools/build/appveyor-ci.bat
@@ -1,5 +1,5 @@
 @echo off
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 set PATH=%QTDIR%\bin;%CD%\bin;%PATH%
 
 mkdir build
